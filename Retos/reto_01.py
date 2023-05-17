e1baff5ea1e68540dfed45f949473b9b9a5d6a81
j4vi3r1 <128844996+j4vi3r1@users.noreply.github.com>
2023-05-16 20:01:56 -0400

    Create reto_01.py

---
+++ Retos/reto_01.py
@@ -0,0 +1,4 @@
+nombre=input("ingresar nombre\n")
+edad=int(input("ingresar edad\n"))
+total_edad= edad+20
+print(f"Hola mi nombre es {nombre}, tengo {edad} años y dentro de 20 años tendre {total_edad} años.")
