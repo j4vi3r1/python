f0eebb96e956bd821377f0e12c1b87a217208670
j4vi3r1 <128844996+j4vi3r1@users.noreply.github.com>
2023-06-02 15:21:19 -0400

    Create ejercicio_01.py

---
+++ parcial02/ejercicio_01.py
@@ -0,0 +1,7 @@
+for numero in range(2,30,3):
+    print(numero)
+
+suma1 = 5+20
+suma2 = 5+11+17+23+29
+print("La suma de los numeros divisibles por '5' es:",suma1)
+print("La suma de los numeros impares es:",suma2)
